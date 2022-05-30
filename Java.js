7c75e7882b35f7f4e59d068f843bbdb672cf0270
NicoDella4 <2114204@ucc.edu.ar>
2022-05-30 20:34:58 -0300

    Se agrego background de rompecabezas

--- Java.js
+++ Java.js
@@ -1,7 +1,5 @@
-importScripts(src="")
 
 function var words = ['VIDON' , 'ANTARES' , 'CASANEGRA' , 'MCDONALD' , 'BURGUERKING', 'ELCLUBDELAMILANESA' , 'MERLOT' ,
                       'SUR' , 'MALBEC' , 'THEBOTTOM' , 'STOWAY' , 'CUATROCATORCE' , 'STANDARD' , 'TOKIN' , 'PANPLANO' ,
                       'BARTO' , 'VLACK' , 'LEPARC' , 'FRATERNO' , 'FOREST' , 'MANDINGA' , 'OCHRE' , 'ROSARIO' , 'PARIS'
                       , 'CRUZESPACIO'];
-        var gamePuzzle = wordfindgame.create(words, '#puzzle-container', '#puzzle-words')
